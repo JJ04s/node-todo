a8768383bef8cf3839dce2ebd088c42b83fb5664
고민재 <minjae_koh@snu.ac.kr>
2026-03-04 22:10:31 +0900

    프로그램 실행 흐름 구성

---
+++ todo.js
@@ -0,0 +1,25 @@
+const command = process.argv[2];
+const taskOrId = process.argv[3];
+
+switch(command){
+    case "add":
+        //addToDo 함수 실행
+        break;
+    
+    case "list":
+        //listToDos 함수 실행
+        break;
+
+    case "done":
+        //doneToDo 함수 실행
+        break;
+
+    case "delete":
+        // delteToDo 함수 실행
+        break;
+
+    case "update":
+        // updateToDo 함수 실행
+        break;
+
+}

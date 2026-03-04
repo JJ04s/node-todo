bd79ffc101f0f0d2c788933a25d64ae42fdf9408
Jaewoong Choi <jwchoi.tech@gmail.com>
2026-03-04 18:32:11 +0900

    feat: command dispatch table 추가
    개발 과정의 어려움
    - 여러 분기 처리를 위해 switch를 사용하려 했으나, 확장성 및 가독성을 dispatch table pattern을 채택

--- todo.js
+++ todo.js
@@ -4,3 +4,11 @@ const [, , command, ...args] = process.argv;
 
 const data = fs.readFileSync("todos.json", "utf8");
 const todos = JSON.parse(data);
+
+const commandHandler = {
+  add: addTodo,
+  delete: deleteTodo,
+  update: updateTodo,
+  done: completeTodo,
+  list: showTodos,
+};

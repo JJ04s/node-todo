87f3f729f40efa8f81cc999d8889517d9c9ae6ed
pkdje <limon@bagdongjes-MacBook-Pro.local>
2026-03-04 19:59:22 +0900

    Write skeleton code, JS를 처음 접하여 문법을 학습하는데 시간이 소요 되었다.

---
+++ todo.js
@@ -0,0 +1,46 @@
+import fs from 'node:fs';
+
+/* 
+TODO list 추가
+추가된 Todo는 고유 ID(1부터 순차 증가)
+추가 후 "Todo가 추가되었습니다: [내용]" 출력
+*/
+
+function add_todo(){}
+
+/* 
+TODO list 조회
+출력 형식은 [ ] 1. 장보기
+완료된 항목은 [x]
+Todo가 없으면 "Todo가 없습니다."
+*/
+function list_todo(){}
+
+/*
+완료 처리
+done [ID] 명령
+처리 후 "[ID]번 항목이 완료되었습니다." 메시지 출력
+존재하지 않는 ID의 경우 "해당 ID를 찾을 수 없습니다." 출력
+*/
+function done_todo(){}
+
+/*
+삭제 기능
+delete [ID]
+delete는 reserved word인데 어떻게 하지?
+process.argv[] 사용자 입력 명령어를 배열로 담아준다.
+*/
+
+function delete_todo(){}
+
+/*
+내용 변경 기능
+update [ID] "새 내용" 명령으로 내용 수정
+*/
+function update_todo(){}
+
+const commands = {}
+
+const command = process.argv[2];
+const id = process.argv[3];
+const content = process.argv[4];

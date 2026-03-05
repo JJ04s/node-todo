58c68ec3de2ecf509b7da58e125c0400a2035fef
Jaewoong Choi <jwchoi.tech@gmail.com>
2026-03-05 15:38:18 +0900

    feat: addTodo, deleteTodo, updateTodo, completeTodo, showTodos 함수 구현
    개발 과정의 어려움
    None

--- todo.js
+++ todo.js
@@ -44,3 +44,49 @@ const handler = commandHandler[command];
 handler(argParser(command, args));
 fs.writeFileSync("todos.json", JSON.stringify(db, null, 2));
 
+function addTodo({ content }) {
+  db.todos.push({
+    id: db.nextId++,
+    content: content,
+    done: false,
+  });
+
+  console.log(`Todo가 추가되었습니다: ${content}`);
+}
+
+function deleteTodo({ id: targetTodoId }) {
+  db.todos = db.todos.filter((todo) => todo.id !== targetTodoId);
+}
+
+function updateTodo({ id: targetTodoId, content: updatedContent }) {
+  const targetTodo = db.todos.find((todo) => todo.id === targetTodoId);
+
+  if (targetTodo) {
+    targetTodo.content = updatedContent;
+  } else {
+    console.log("해당 ID를 찾을 수 없습니다.");
+  }
+}
+
+function completeTodo({ id: targetTodoId }) {
+  const targetTodo = db.todos.find((todo) => todo.id === targetTodoId);
+
+  if (targetTodo) {
+    targetTodo.done = true;
+  } else {
+    console.log("해당 ID를 찾을 수 없습니다.");
+  }
+}
+
+function showTodos() {
+  if (db.todos.length === 0) {
+    console.log("Todo가 없습니다.");
+    return;
+  }
+
+  const todoList = db.todos
+    .map((todo) => `[${todo.done ? "X" : " "}] ${todo.id}. ${todo.content}`)
+    .join("\n");
+
+  console.log(todoList);
+}

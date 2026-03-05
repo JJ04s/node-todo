3b2edf35ac8a7d01562ef559c3856b375fc6f4ab
Jaewoong Choi <jwchoi.tech@gmail.com>
2026-03-05 16:46:07 +0900

    feat: 에러 케이스 추가
    개발 과정의 어려움
    None

--- todoService.js
+++ todoService.js
@@ -1,6 +1,13 @@
 export default function createTodoService(db) {
   return {
     addTodo({ content }) {
+      if (content.trim() === "") {
+        return {
+          mutated: false,
+          message: "추가할 Todo를 입력해주세요",
+        };
+      }
+
       db.todos.push({
         id: db.nextId++,
         content: content,
@@ -14,6 +21,13 @@ export default function createTodoService(db) {
     },
 
     deleteTodo({ id: targetTodoId }) {
+      if (Number.isNaN(targetTodoId)) {
+        return {
+          mutated: false,
+          message: "삭제할 Todo의 ID를 입력해 주세요.",
+        };
+      }
+
       const targetTodoIndex = db.todos.findIndex(
         (todo) => todo.id === targetTodoId,
       );
@@ -33,6 +47,19 @@ export default function createTodoService(db) {
     },
 
     updateTodo({ id: targetTodoId, content: updatedContent }) {
+      if (Number.isNaN(targetTodoId)) {
+        return {
+          mutated: false,
+          message: "수정할 Todo의 ID를 입력해 주세요.",
+        };
+      }
+      if (updatedContent.trim() === "") {
+        return {
+          mutated: false,
+          message: "수정할 Todo를 입력해주세요",
+        };
+      }
+
       const targetTodo = db.todos.find((todo) => todo.id === targetTodoId);
 
       if (targetTodo) {
@@ -50,14 +77,28 @@ export default function createTodoService(db) {
     },
 
     completeTodo({ id: targetTodoId }) {
+      if (Number.isNaN(targetTodoId)) {
+        return {
+          mutated: false,
+          message: "완료할 Todo의 ID를 입력해 주세요.",
+        };
+      }
+
       const targetTodo = db.todos.find((todo) => todo.id === targetTodoId);
 
       if (targetTodo) {
-        targetTodo.done = true;
-        return {
-          mutated: true,
-          message: `ID ${targetTodoId}번 항목이 완료되었습니다.`,
-        };
+        if (targetTodo.done) {
+          return {
+            mutated: false,
+            message: "이미 완료된 Todo입니다.",
+          };
+        } else {
+          targetTodo.done = true;
+          return {
+            mutated: true,
+            message: `ID ${targetTodoId}번 항목이 완료되었습니다.`,
+          };
+        }
       } else {
         return {
           mutated: false,

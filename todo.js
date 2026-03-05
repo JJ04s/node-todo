3388b46daea05d44d543f5ae7111fc2794b0491b
Jaewoong Choi <jwchoi.tech@gmail.com>
2026-03-05 15:36:36 +0900

    feat: argParser 추가 및 file IO 구현
    개발 과정의 어려움
    - command에 따라 args의 개수가 달라지는데, 이를 일관적으로 처리하기 위한 방법을 고민하였음.
    - parsedArgs라는 객체에 args를 담아서 통째로 넘기고, 각 함수에서 args를 처리하는 식으로 구현함

--- todo.js
+++ todo.js
@@ -12,3 +12,35 @@ const commandHandler = {
   done: completeTodo,
   list: showTodos,
 };
+
+function argParser(command, args) {
+  const parsedArgs = {};
+
+  switch (command) {
+    case "add":
+      parsedArgs.content = args.join(" ");
+      break;
+    case "delete":
+      parsedArgs.id = Number(args[0]);
+      break;
+    case "update":
+      const [id, ...content] = args;
+      parsedArgs.id = Number(id);
+      parsedArgs.content = content.join(" ");
+      break;
+    case "done":
+      parsedArgs.id = Number(args[0]);
+      break;
+    case "list":
+      break;
+    default:
+      console.log("알 수 없는 command입니다.");
+  }
+
+  return parsedArgs;
+}
+
+const handler = commandHandler[command];
+handler(argParser(command, args));
+fs.writeFileSync("todos.json", JSON.stringify(db, null, 2));
+

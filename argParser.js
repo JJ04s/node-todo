047c5f024bbe3688f6a59dad4026568aac52b8c9
Jaewoong Choi <jwchoi.tech@gmail.com>
2026-03-05 15:57:32 +0900

    refactor: argParser 분리
    개발 과정의 어려움
    None

---
+++ argParser.js
@@ -0,0 +1,26 @@
+export default function argParser(command, args) {
+  const parsedArgs = {};
+
+  switch (command) {
+    case "add":
+      parsedArgs.content = args.join(" ");
+      break;
+    case "delete":
+      parsedArgs.id = Number(args[0]);
+      break;
+    case "update":
+      const [id, ...content] = args;
+      parsedArgs.id = Number(id);
+      parsedArgs.content = content.join(" ");
+      break;
+    case "done":
+      parsedArgs.id = Number(args[0]);
+      break;
+    case "list":
+      break;
+    default:
+      console.log("알 수 없는 command입니다.");
+  }
+
+  return parsedArgs;
+}
--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import createTodoService from "./todoService.js";
+import argParser from "./argParser.js";
 
 const [, , command, ...args] = process.argv;
 
@@ -18,30 +19,3 @@ const commandHandler = {
 const handler = commandHandler[command];
 handler(argParser(command, args));
 fs.writeFileSync("todos.json", JSON.stringify(db, null, 2));
-
-function argParser(command, args) {
-  const parsedArgs = {};
-
-  switch (command) {
-    case "add":
-      parsedArgs.content = args.join(" ");
-      break;
-    case "delete":
-      parsedArgs.id = Number(args[0]);
-      break;
-    case "update":
-      const [id, ...content] = args;
-      parsedArgs.id = Number(id);
-      parsedArgs.content = content.join(" ");
-      break;
-    case "done":
-      parsedArgs.id = Number(args[0]);
-      break;
-    case "list":
-      break;
-    default:
-      console.log("알 수 없는 command입니다.");
-  }
-
-  return parsedArgs;
-}

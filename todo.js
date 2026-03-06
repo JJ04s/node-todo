0714238cfa68a68ced6d2c16c422fba51219aec5
정현우 <exile1111@snu.ac.kr>
2026-03-06 16:11:10 +0900

    add 명령어 추가
    어려운 점 : JS를 처음으로 만지면서 원하는 결과를 위한 함수를 찾는 과정이
    어려웠다. id가 완전히 구현되지 않았으나 임시로 값을 지정했다

--- todo.js
+++ todo.js
@@ -1,11 +1,21 @@
 import fs from 'node:fs'; // 파일 제어 도구 불러오기
 
-const task = "Node.js 공부하기";
+const args = process.argv;
+const command = args[2];
+const text = args[3];
+let id = 1;
 
-// 1. 할 일을 todo.txt 파일에 저장 (쓰기)
-fs.writeFileSync('todo.txt', task);
-console.log('할 일이 저장되었다.');
+let todos = [];
+if (fs.existsSync('todos.json')) {
+    const data = fs.readFileSync('todos.json', 'utf-8');
+    todos = JSON.parse(data);
+}
+if (command === 'add') {
+    const newTodo = {
+        id: id++, content : text, done : false
+    };
+    todos.push(newTodo);
 
-// 2. 저장된 내용을 읽어오기 (읽기)
-const data = fs.readFileSync('todo.txt', 'utf-8');
-console.log('파일에서 읽은 할 일:', data);
+    fs.writeFileSync('todos.json', JSON.stringify(todos));
+    console.log('Todo가 추가되었습니다: ['+text+']');
+}

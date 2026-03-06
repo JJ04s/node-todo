ee209e7838b25359c1a1924a319fc45eb08244bd
정현우 <exile1111@snu.ac.kr>
2026-03-06 16:45:30 +0900

    Done 기능 구현, 에러처리
    어려운 점 : find 함수의 문법이  생소하여 고민되었다. todos.json을
    비운 후 list를 체크하였더니 에러가 나 함수를 찾는 과정이 시간이 걸렸다.

--- todo.js
+++ todo.js
@@ -6,28 +6,36 @@ const text = args[3];
 let id = 1;
 
 let todos = [];
-if (fs.existsSync('todos.json')) {
-    const data = fs.readFileSync('todos.json', 'utf-8');
-    todos = JSON.parse(data);
+try {
+    if (fs.existsSync('todos.json')) {
+        const data = fs.readFileSync('todos.json', 'utf-8');
+        todos = JSON.parse(data);
+    } // todos에 todos.json의 내용을 불러와서 저장
+} catch (e) {
+    todos = [];
 }
 if (command === 'add') {
     const newTodo = {
         id: id++, content : text, done : false
     };
-    todos.push(newTodo);
+    todos.push(newTodo); // newTodo 객체를 todos 배열에 추가
 
-    fs.writeFileSync('todos.json', JSON.stringify(todos));
+    fs.writeFileSync('todos.json', JSON.stringify(todos)); // todos 배열을 todos.json에 저장
     console.log('Todo가 추가되었습니다: ['+text+']');
 } else if (command === 'list') {
     let todoslist = [];
-    const data = fs.readFileSync('todos.json', 'utf-8');
-    todoslist = JSON.parse(data);
-
-    if (todoslist.length === 0) {
+    try { //todos.json의 내용을 todoslist에 저장
+        const data = fs.readFileSync('todos.json', 'utf-8');
+        todoslist = JSON.parse(data);
+    } catch (e) {
+    // 파일이 비어있음
+    todoslist = [];
+    }
+    if (todoslist.length === 0) { // todoslist가 비어있다면 종료
         console.log('Todo가 없습니다.');
     } else {
         while(todoslist.length > 0) {
-            const todo = todoslist.shift();
+            const todo = todoslist.shift(); //todoslist의 요소를 todo에 전달하고 삭제
             const status = todo.done 
             if(status === true) {
                 console.log('[x] ' + id.toString() + '. ' + todo.content);
@@ -36,5 +44,24 @@ if (command === 'add') {
                 console.log('[ ] ' + id.toString() + '. ' + todo.content);
             }
         }
+    }
+} else if (command === 'done') {
+    let todoslist = [];
+    try { //todos.json의 내용을 todoslist에 저장
+        const data = fs.readFileSync('todos.json', 'utf-8');
+        todoslist = JSON.parse(data);
+    } catch (e) {
+    // 파일이 비어있음
+    todoslist = [];
+    }
+    const item = todoslist.find((todo) => todo.id === parseInt(text)); //text와 일치하는 todo 찾기
+    
+    if(item) {
+        item.done = true;
+
+        fs.writeFileSync('todos.json', JSON.stringify(todoslist));
+        console.log('ID ' + text + '번 항목이 완료되었습니다.');
+    } else {
+        console.log('해당 ID를 찾을 수 없습니다.');
     }
 }

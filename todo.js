5879702004d110a0d9b0fbc1434316f2e6eeb33d
정현우 <exile1111@snu.ac.kr>
2026-03-06 17:06:14 +0900

    undo, delete, undate 명령어 추가
    어려운 점 : 앞의 코드를 재사용하여 만들었다. JS가 익숙하지 않아 splice 함수를 사용하는 방법을 고민하였다.

--- todo.js
+++ todo.js
@@ -3,6 +3,7 @@ import fs from 'node:fs'; // 파일 제어 도구 불러오기
 const args = process.argv;
 const command = args[2];
 const text = args[3];
+const text2 = args[4];
 let id = 1;
 
 let todos = [];
@@ -61,6 +62,62 @@ if (command === 'add') {
 
         fs.writeFileSync('todos.json', JSON.stringify(todoslist));
         console.log('ID ' + text + '번 항목이 완료되었습니다.');
+    } else {
+        console.log('해당 ID를 찾을 수 없습니다.');
+    }
+} else if (command === 'undo') {
+    let todoslist = [];
+    try { //todos.json의 내용을 todoslist에 저장
+        const data = fs.readFileSync('todos.json', 'utf-8');
+        todoslist = JSON.parse(data);
+    } catch (e) {
+    // 파일이 비어있음
+    todoslist = [];
+    }
+    const item = todoslist.find((todo) => todo.id === parseInt(text)); //text와 일치하는 todo 찾기
+    
+    if(item) {
+        item.done = false;
+
+        fs.writeFileSync('todos.json', JSON.stringify(todoslist));
+        console.log('ID ' + text + '번 항목이 취소되었습니다.');
+    } else {
+        console.log('해당 ID를 찾을 수 없습니다.');
+    }
+} else if (command === 'delete') {
+    let todoslist = [];
+    try { //todos.json의 내용을 todoslist에 저장
+        const data = fs.readFileSync('todos.json', 'utf-8');
+        todoslist = JSON.parse(data);
+    } catch (e) {
+    // 파일이 비어있음
+    todoslist = [];
+    }
+    const item = todoslist.find((todo) => todo.id === parseInt(text)); //text와 일치하는 todo 찾기
+    
+    if(item) {
+        todoslist.splice(todoslist.indexOf(item), 1); //todoslist에서 해당 요소 삭제
+        fs.writeFileSync('todos.json', JSON.stringify(todoslist));
+        console.log('ID ' + text + '번 항목이 삭제되었습니다.');
+    } else {
+        console.log('해당 ID를 찾을 수 없습니다.');
+    }
+} else if (command == 'update') {
+    let todoslist = [];
+    try { //todos.json의 내용을 todoslist에 저장
+        const data = fs.readFileSync('todos.json', 'utf-8');
+        todoslist = JSON.parse(data);
+    } catch (e) {
+    // 파일이 비어있음
+    todoslist = [];
+    }
+    const item = todoslist.find((todo) => todo.id === parseInt(text)); //text와 일치하는 todo 찾기
+    
+    if(item) {
+        item.content = args[4]; //content 업데이트
+        item.done = false; //done 초기화
+        fs.writeFileSync('todos.json', JSON.stringify(todoslist));
+        console.log('ID ' + text + '번 항목이 변경되었습니다.');
     } else {
         console.log('해당 ID를 찾을 수 없습니다.');
     }

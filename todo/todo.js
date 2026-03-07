d6196a832aa3471433b5e9bb41afe7b9ee0823a8
오수현 <ohsuhyeon@osuhyeon-ui-MacBookPro.local>
2026-03-07 21:39:28 +0900

    add: complete add_todo - 새 id를 어떤식으로 받아와야 할지 떠올리는게 어려웠음

--- todo/todo.js
+++ todo/todo.js
@@ -1,5 +1,85 @@
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
+const FILE = path.join(__dirname, 'todos.json');
 
-const TODO_FILE = path.join(__dirname, 'todos.json');
+function add_todo(content) {
 
+  if (!content) {
+    console.log("할 일 내용을 입력하세요.");
+    return;
+  }
+
+  let todos = [];
+
+  if (fs.existsSync(FILE)) {
+    const data = fs.readFileSync(FILE, 'utf8');
+    todos = JSON.parse(data);
+  }
+
+  const newId = todos.length === 0 ? 1 : todos[todos.length - 1].id + 1;
+
+  const newTodo = {
+    id: newId,
+    content: content,
+    done: false
+  };
+
+  todos.push(newTodo);
+
+  fs.writeFileSync(FILE, JSON.stringify(todos, null, 2));
+
+  console.log(`Todo가 추가되었습니다: ${content}`);
+}
+
+function list_todos() {
+
+}
+
+function done_todo(id) {
+
+}
+
+function delete_todo(id) {
+
+}
+
+function update_todo(id, new_content) {
+
+}
+
+function print_usage() {
+
+}
+
+
+function main() {
+  const command = process.argv[2];
+
+  switch (command) {
+    case 'add': {
+      add_todo(process.argv[3]);
+      break;
+    }
+    case 'list': {
+      list_todos();
+      break;
+    }
+    case 'done': {
+      done_todo(process.argv[3]);
+      break;
+    }
+    case 'delete': {
+      delete_todo(process.argv[3]);
+      break;
+    }
+    case 'update': {
+      update_todo(process.argv[3],process.argv[4]);
+      break;
+    }
+    default: {
+      print_usage();
+    }
+  }
+}
+
+main();

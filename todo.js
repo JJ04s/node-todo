2f8497e76e4473b768b956ceb62092d5a93f95bc
motoo42 <motoo42@snu.ac.kr>
2026-03-08 03:00:46 +0900

    함수를 작성하고 기능에 맞게 구현하는 과정에서 자바스크립트에 익숙치 않아 여러 시행착오를 거쳤음.

--- todo.js
+++ todo.js
@@ -1,1 +1,30 @@
 const fs = require('fs');
+const command = process.argv[2];
+const param = process.argv[3];
+
+function readTodos() {
+    const data = fs.readFileSync('todos.json', 'utf8');
+    return JSON.parse(data);
+}
+
+function listTodos() {
+    const todos = readTodos();
+
+    if (todos.length === 0) {
+        console.log("Todo가 없습니다.");
+        return;
+    }
+
+    todos.forEach(todo => {
+        const status = todo.done ? "[x]" : "[]";
+        console.log(`${status} ${todo.id}. ${todo.content}`);
+    });
+}
+
+switch (command) {
+    case 'list':
+        listTodos();
+        break;
+    default:
+        console.log("wrong command");
+}

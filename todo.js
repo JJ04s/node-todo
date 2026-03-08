a6277b797cf9a986ec476fc6f8a6e5472f4e89ed
Kyungjin (James) Kim <kkjjames0617@gmail.com>
2026-03-08 22:55:59 +0900

    feat: delete function

--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
 import { get } from 'node:http';
+import { json } from 'node:stream/consumers';
 import { todo } from 'node:test';
 
 const FILE_TODO = 'todo.json'
@@ -69,5 +70,25 @@ if (command === 'done') {
         target.done = true;
         fs.writeFileSync(FILE_TODO, JSON.stringify(fileJson, null, 2));
         console.log(`Task ${target.id}: "${target.content}" done.`);
+    }
+}
+
+if (command === 'delete') {
+    const targetId = parseInt(args[1]);
+    
+    if (!targetId) {
+        console.log('No task ID provided.');
+        process.exit(1);
+    }
+
+    const fileJson = getData();
+    const target = fileJson.find(t => t.id === targetId);
+
+    if (!target) {
+        console.log(`Task ID${targetId} not found`);
+    } else {
+        const newFileJson = fileJson.filter(t => t.id !== targetId);
+        fs.writeFileSync(FILE_TODO, JSON.stringify(fileJson, null, 2));
+        console.log(`Task ${target.id}: "${target.content}" deleted.`);
     }
 }

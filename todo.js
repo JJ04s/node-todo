1f1fc8968c27d7e95bf4756a03cb7e83dddddc79
LEESEOKWOO <spssseok@snu.ac.kr>
2026-03-08 16:54:59 +0900

    addTodo 기능 추가 및 JSON.parse 에러 해결 / todos.json 파일이 비어있어 에러 발생)

--- todo.js
+++ todo.js
@@ -2,22 +2,43 @@ const fs = require('fs');
 
 
 
-function addTodo(newContent){
+function addTodo(Content){
     const data = fs.readFileSync('todos.json', 'utf8');
     const todos = JSON.parse(data);
 
-    const 
+    const newID = todos.length > 0 ? todos[todos.length-1].id + 1 : 1;
+
+    todos.push({id: newID, content : Content, done : false});
+
+    fs.writeFileSync('todos.json', JSON.stringify(todos, null, 2));
+}
+
+function listTodo(){
+
+}
+
+function doneTodo(){
+
+}
+
+function deleteTodo(){
+
+}
+
+function updateTodo(){
+
 }
 
 function main(){
     const command = process.argv[2]; const content = process.argv[3];
     switch (command){
         case 'add' :{
-
+            addTodo(content);
+            break;
         }
 
         case 'list' :{
-            
+            listTodo();
         }
 
         case 'done' :{
@@ -29,6 +50,9 @@ function main(){
         }
 
         case 'update' :{
+
+        }
+        default:{
 
         }
     }

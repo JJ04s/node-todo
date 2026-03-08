2d2cd0294955e86a4b4cc737c8423ddad469c0de
양해울 <haeul1005@naver.com>
2026-03-08 17:32:45 +0900

    feat: update todo

--- todo.js
+++ todo.js
@@ -31,7 +31,7 @@ switch (command) {
             console.log("Todo가 없습니다.");
         } else {
             todos.forEach(todo => {
-                let todoDone = todo.done ? ' ' : 'x'; // 완료되지 않은 할 일에 대해서만 'x' 저장
+                let todoDone = todo.done ? 'x' : ' '; // 완료된 일에 대해서만 'x' 저장
                 console.log("[%s] %s. %s", todoDone, todo.id, todo.content); // todo 리스트 출력
             });
         }
@@ -39,17 +39,24 @@ switch (command) {
     
     case "done":
     case "delete":
-        const targetId = parseInt(args[1]); // 완료/삭제 처리할 ID
+    case "update":
+        const targetId = parseInt(args[1]); // 완료/삭제/수정 처리할 ID
         const todoIndex = todos.findIndex(t => t.id === targetId); // 저장된 json 파일에서 인덱스 찾기 (존재하지 않으면 -1)
         if(isNaN(targetId) || todoIndex === -1) {
             console.log("해당 ID를 찾을 수 없습니다.");
         } else {
             if(command === "done") {
                 todos[todoIndex].done = true; // 완료 처리
-                console.log("ID %d번 항목이 완료되었습니다.", targetId)
-            } else {
+                console.log("ID %d번 항목이 완료되었습니다.", targetId);
+            } else if(command === "delete") {
                 todos.splice(todoIndex, 1); // 삭제 처리
-                console.log("ID %d번 항목이 삭제되었습니다.", targetId)
+                console.log("ID %d번 항목이 삭제되었습니다.", targetId);
+            } else {
+                const newTask = args[2]; // 새로운 할 일 내용
+                if(!isEmpty(newTask)) {
+                    todos[todoIndex].content = newTask;
+                    console.log("ID %d번 항목이 수정되었습니다: %s", targetId, newTask);
+                }
             }
             fs.writeFileSync('todos.json', JSON.stringify(todos, null, 2));
         }

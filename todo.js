d65fe53b09eacc5448bf64cc3b23b36379a060e1
Kyungjin (James) Kim <kkjjames0617@gmail.com>
2026-03-08 22:51:35 +0900

    feat: done function

--- todo.js
+++ todo.js
@@ -1,4 +1,5 @@
 import fs from 'node:fs';
+import { get } from 'node:http';
 import { todo } from 'node:test';
 
 const FILE_TODO = 'todo.json'
@@ -48,5 +49,25 @@ if (command === 'list') {
             const status = todo.done ? '[X]' : '[ ]';
             console.log(`${status} ${todo.id}. ${todo.content}`);
         });
+    }
+}
+
+if (command === 'done') {    
+    const targetId = parseInt(args[1]);
+    
+    if (!targetId) {
+        console.log('No task ID provided.');
+        process.exit(1);
+    }
+
+    const fileJson = getData();
+    const target = fileJson.find(t => t.id === targetId);
+
+    if (!target) {
+        console.log(`Task ID${targetId} not found`);
+    } else {
+        target.done = true;
+        fs.writeFileSync(FILE_TODO, JSON.stringify(fileJson, null, 2));
+        console.log(`Task ${target.id}: "${target.content}" done.`);
     }
 }

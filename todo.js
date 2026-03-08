1d95b02b93ec01254b368f2f85e9d982e2c797f0
mgahn0706 <mgahn0706@snu.ac.kr>
2026-03-08 15:26:14 +0900

    feat: add functionality to save and add todos - 어려운 점: id를 단순히 length +1로 하면 유일한 id가 아니게 되어 이후 undo 기능들을 추가하게 될 때 문제가 생길 수 있음

--- todo.js
+++ todo.js
@@ -2,13 +2,32 @@ import fs from "node:fs";
 
 const FILE_PATH = "todos.json";
 
-function loadTodos() {
+const loadTodos = () => {
   if (!fs.existsSync(FILE_PATH)) {
     return [];
   }
   const data = fs.readFileSync(FILE_PATH, "utf-8");
   return JSON.parse(data);
-}
+};
+
+const saveTodos = (todos) => {
+  if (!Array.isArray(todos)) {
+    throw new Error("Todos should be an array");
+  }
+  fs.writeFileSync(FILE_PATH, JSON.stringify(todos, null, 2), "utf-8");
+};
+
+const addTodo = (content) => {
+  const todos = loadTodos();
+  const newTodo = {
+    id: todos.length + 1,
+    content,
+    done: false,
+  };
+  todos.push(newTodo);
+  saveTodos(todos);
+  console.log(`Todo가 추가되었습니다: [${title}]`);
+};
 
 const [, , command, ...args] = process.argv;
 

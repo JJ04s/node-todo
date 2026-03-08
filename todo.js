87980b230cff83ad2b9b4265b35399e2bbdcd08b
Kyungjin (James) Kim <kkjjames0617@gmail.com>
2026-03-08 22:40:56 +0900

    feat: list function

--- todo.js
+++ todo.js
@@ -1,24 +1,28 @@
 import fs from 'node:fs';
+import { todo } from 'node:test';
 
 const FILE_TODO = 'todo.json'
 
 const args = process.argv.slice(2);
 const command = args[0];
-const task = args.slice(1).join('');
 
 if (!fs.existsSync(FILE_TODO)) {
     fs.writeFileSync(FILE_TODO, JSON.stringify([]));
     console.log('Created todo.js')
 }
 
+const getData = () => {
+    const data = fs.readFileSync(FILE_TODO, 'utf-8');
+    return JSON.parse(data)
+};
+
 if (command === 'add') {
+    const task = args.slice(1).join('');
     if (!task) {
         console.log('No task provided.');
         process.exit(1);
     }
-    const fileData = fs.readFileSync(FILE_TODO, 'utf-8');
-    const fileJson = JSON.parse(fileData);
-
+    const fileJson = getData();
     const newId = (fileJson.length > 0 ? Math.max(...fileJson.map(t => t.id)) : 0) + 1; 
 
     const newTask = {
@@ -31,4 +35,18 @@ if (command === 'add') {
     fs.writeFileSync(FILE_TODO, JSON.stringify(fileJson, null, 2));
     
     console.log(`Added: "${task}" (ID: ${newId})`);
+}
+
+if (command === 'list') {
+    const fileJson = getData();
+
+    if (fileJson.length ===0 ) {
+        console.log('TODO list is empty.');
+    } else {
+        console.log('<< TODO list >>');
+        fileJson.forEach(todo => {
+            const status = todo.done ? '[X]' : '[ ]';
+            console.log(`${status} ${todo.id}. ${todo.content}`);
+        });
+    }
 }

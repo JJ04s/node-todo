997a8fe19c0845fae5be7a6c2101dc796ad52016
LEESEOKWOO <spssseok@snu.ac.kr>
2026-03-08 17:12:58 +0900

    listTodo 기능 추가 / JS for 문법에서 어려움 겪음)

--- todo.js
+++ todo.js
@@ -3,6 +3,12 @@ const fs = require('fs');
 
 
 function addTodo(Content){
+
+    if(!Content){
+        console.log('에러 : 할 일을 추가하세요');
+        return;
+    }
+
     const data = fs.readFileSync('todos.json', 'utf8');
     const todos = JSON.parse(data);
 
@@ -11,10 +17,25 @@ function addTodo(Content){
     todos.push({id: newID, content : Content, done : false});
 
     fs.writeFileSync('todos.json', JSON.stringify(todos, null, 2));
+
+    console.log(`Todo가 추가되었습니다: [${Content}]`);
+    return;
 }
 
-function listTodo(){
+function listTodo() {
+    const data = fs.readFileSync('todos.json', 'utf8');
+    const todos = JSON.parse(data);
 
+    if (todos.length === 0) {
+        console.log("Todo가 없습니다.");
+        return;
+    }
+
+    todos.forEach(todo => {
+        const status = todo.done ? "[x]" : "[ ]";
+        
+        console.log(`${status} ${todo.id}. ${todo.content}`);
+    });
 }
 
 function doneTodo(){
@@ -39,6 +60,7 @@ function main(){
 
         case 'list' :{
             listTodo();
+            break;
         }
 
         case 'done' :{
